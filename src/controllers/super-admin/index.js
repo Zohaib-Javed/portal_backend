270b2f9a75ef981337a9ffcbc8172229323ed9b0
Zohaib Javed <zohaibjaved2002@gmail.com>
2021-04-20 13:54:16 +0500

    validations of user and admin

--- src/controllers/super-admin/index.js
+++ src/controllers/super-admin/index.js
@@ -10,42 +10,44 @@ const {addPayment}=require('./add-payment');
 
 const superAdminDataValidation=(path)=>{
   switch (path) {
-	case 'create-grade':
-		return [
-			check('email',"Email is missing").exists(),
-		  check('email', 'Invalid email.').isEmail().normalizeEmail(),
-		  check('role','Invalid role.').isIn(['admin','teacher','student']),
-		 
-		];
-	case 'create-user':
-	  return [
-		check('email', 'Invalid email.').isEmail().normalizeEmail(),
-		check('role','Invalid role.').isIn(['admin','teacher','student']),
-	   
-	  ];
-	case 'create-payment':
-	return [
-	check('email', 'Invalid email.').isEmail().normalizeEmail(),
-	check('role','Invalid role.').isIn(['admin','teacher','student']),
-	
-	];
-	case 'create-dues':
-	return [
-		check('email', 'Invalid email.').isEmail().normalizeEmail(),
-		check('role','Invalid role.').isIn(['admin','teacher','student']),
-		
-	];
-	default:
-	  break;
-  }
+		case 'create-grade':
+			return [
+				check("gradeNum","Grade Number is missing").exists(),
+			];
+		case 'create-user':
+			return [
+				check("email","Email is missing").exists(),
+				check('email', 'Invalid email.').isEmail().normalizeEmail(),
+				check('name', 'Name is missing.').exists(),
+				check('password', 'Password is missing.').exists(),
+				check('password','Password must atleast be 6 characters long').isLength({ min: 6 }),
+				check('role','Invalid role.').isIn(['admin','teacher','student']),
+			
+			];
+		case 'create-payment':
+			return [
+				check('amount', 'Amount is missing.').exists(),
+				check('date','Date is missing.').exists(),
+				check('duePaymentId', 'User Id is missing.').exists(),
+			];
+		case 'create-dues':
+			return [
+				check('amount', 'Amount is missing.').exists(),
+				check('dueDate','Due Date is missing.').exists(),
+				check('userId', 'User Id is missing.').exists(),
+				check('isPaid', 'Is Paid is missing.').exists(),
+			];
+		default:
+			break;
+		}
 }
 module.exports = (services) => {
 	const api = router();
 
-	api.post('/create-grade',createGrades(services));   
+	api.post('/create-grade',superAdminDataValidation("create-grade"),validateData,createGrades(services));   
 	api.post('/create-users',superAdminDataValidation("create-user"),validateData,createUsers(services)); 
-	api.post('/create-payment',addPayment(services));
-	api.post('/create-dues',addDuePayment(services));  
+	api.post('/create-payment',superAdminDataValidation("create-payment"),validateData,addPayment(services));
+	api.post('/create-dues',superAdminDataValidation("create-dues"),validateData,addDuePayment(services));  
 
 
 	return api;
--- src/controllers/user/index.js
+++ src/controllers/user/index.js
@@ -1,11 +1,37 @@
 var express = require('express');
 var router = express.Router();
-
+const { check } = require('express-validator');
+const {validateData}=require('../../middlewares');
 const { signIn } = require('./sign-in');
 const {createSuperAdmin}=require('./create-super-admin');
+
+const superAdminDataValidation=(path)=>{
+  switch (path) {
+		case 'sign-in':
+			return [
+				check("email","Email is missing").exists(),
+				check('email', 'Invalid email.').isEmail().normalizeEmail(),
+				check('password', 'Password is missing.').exists(),
+				check('password','Password must atleast be 6 characters long').isLength({ min: 6 }),
+			];
+		case 'create-super-admin':
+			return [
+				check("email","Email is missing").exists(),
+				check('email', 'Invalid email.').isEmail().normalizeEmail(),
+				check('name', 'Name is missing.').exists(),
+				check('password', 'Password is missing.').exists(),
+				check('password','Password must atleast be 6 characters long').isLength({ min: 6 }),
+				check('role','Role is missing.').exists(),
+				check('role','Invalid role.').isIn(['admin','teacher','student']),
+			
+			];
+		default:
+			break;
+		}
+}
 module.exports = (services) => {
-    router.post('/sign-in',signIn(services));  
-    router.post('/create-super-admin',createSuperAdmin(services)); 
+    router.post('/sign-in',superAdminDataValidation("sign-in"),validateData,signIn(services));  
+    router.post('/create-super-admin',superAdminDataValidation("create-super-admin"),validateData,createSuperAdmin(services)); 
 
     return router;
 };
